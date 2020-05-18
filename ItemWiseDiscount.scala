dcd85a0949ded8c2642565d42704c312255b8ec0
Hari Krishna N <52846376+hnadimint@users.noreply.github.com>
2020-05-19 02:16:16 +0530

    caching\aggregate.ItemWiseDiscount
    caching\aggregate.ItemWiseDiscount

--- ItemWiseDiscount.scala
+++ ItemWiseDiscount.scala
@@ -1,30 +1,44 @@
-package aggregate
+package aggregate.caching
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
+
+case class SalesRecord(transactionid: Int, customerid: Int,
+                 itemid: Int, itemprice: Double)
 
 object ItemWiseDiscount {
+
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName(getClass.getName)
+    val conf = new SparkConf()
+      .setAppName(getClass.getName)
       .setMaster("local")
+
     val sc = new SparkContext(conf)
-    val salesRDD = sc.textFile(args(0))
-    salesRDD.foreach(r => println(r))
 
-    val salesPairRDD = salesRDD.map(rec => {
+    val fileRDD = sc.textFile(args(0))
+
+    // impose schema/structure on the raw data using a case class
+    val salesRDD = fileRDD.map(rec => {
       val fieldArr = rec.split(",")
-      (fieldArr(1).toInt, fieldArr(3).toDouble)
+      val sr = SalesRecord(fieldArr(0).toInt, fieldArr(1).toInt, fieldArr(2).toInt, fieldArr(3).toDouble)
+      sr
     })
-    salesPairRDD.foreach(r => println(r))
-    val DiscountRDD = salesPairRDD.map(t => {
-      val amount =
-        0.95 * t._2
-      (t._1, amount)
-    })
-    DiscountRDD.foreach(r => println(r))
-    val totalAmountRDD = DiscountRDD.reduceByKey(_ + _)
 
-    totalAmountRDD.foreach(r => println(r))
+    /*salesRDD.foreach(sr => println(sr.customerid,sr.itemprice))
+    salesRDD.foreach(sr => println(sr.customerid,sr.itemid))
+    salesRDD.foreach(sr => println(sr.customerid,sr.transactionid))*/
+    val salesPairRDD = salesRDD.map(sr => (sr.customerid, sr.itemprice * 0.95))
 
+    val totalAmountRDD = salesPairRDD.reduceByKey(_ + _)
+    totalAmountRDD.persist(StorageLevel.MEMORY_ONLY_SER)
 
+    println(totalAmountRDD.collect.toList)
+    totalAmountRDD.foreach(println)
+
+    // flush off from the memory (or) clear RDD from the cache memory
+    totalAmountRDD.unpersist()
+    salesPairRDD.cache
+    Thread.sleep(300000)
   }
+
 }
